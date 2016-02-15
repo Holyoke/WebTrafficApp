67eb0470c265e33578a55937ee8b7f0d0b154691
Holyoke <kid.idioteque@gmail.com>
2016-02-15 11:15:12 -0800

    Basic table setup using Reactable

--- frontend/components/websites_list.jsx
+++ frontend/components/websites_list.jsx
@@ -1,9 +1,27 @@
-var React = require('react');
+var React = require('react'),
+    WebsiteListingStore = require('../stores/website_listing_store.js'),
+    Table = require('reactable').Table;
 
 var WebsitesList = React.createClass({
+  getInitialState: function () {
+    return ({websiteListings: WebsiteListingStore.all()});
+  },
+
+  websiteListingsChanged: function () {
+    var self = this;
+    self.setState({websiteListings: WebsiteListingStore.all()});
+  },
+
+  componentDidMount: function () {
+    WebsiteListingStore.addChangedHandler(this.websiteListingsChanged);
+    WebsiteListingStore.fetch();
+  },
+
   render: function () {
+    var data = this.state.websiteListings;
+
     return(
-      <p>Websites List</p>
+      <Table className="table" data={data} />
     )
   }
 });
